c216bcbcbf92a0e0e26ecdb64b3b08e29d6c2451
denizece <aktandenizece89@gmail.com>
2017-06-20 23:41:53 +0300

    Add files via upload

--- util/Coordinate.java
+++ util/Coordinate.java
@@ -1,1 +1,25 @@
-a
+package util;
+
+public class Coordinate {
+private double x;
+private double y;
+public Coordinate(double nextNumber, double nextNumber2) {
+	x = nextNumber;
+	y = nextNumber2;
+}
+public Coordinate() {
+	// TODO Auto-generated constructor stub
+}
+public double getX() {
+	return x;
+}
+public void setX(double x) {
+	this.x = x;
+}
+public double getY() {
+	return y;
+}
+public void setY(double y) {
+	this.y = y;
+}
+}
